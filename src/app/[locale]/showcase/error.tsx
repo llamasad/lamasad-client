530934b99ab2099efc69efbc2614f993d9dbf036
datdo <datdo27120003@gmail.cco>
2024-06-25 17:39:00 +0700

    131231

--- src/app/[locale]/showcase/error.tsx
+++ src/app/[locale]/showcase/error.tsx
@@ -1,6 +1,7 @@
 // error.jsx
 'use client';
 
-export default function Error() {
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+    console.log(error);
     return <div>Oops, error!</div>;
 }
